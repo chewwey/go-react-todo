e856151fc5126f88d0ba819beebc0eec2f92b6de
NotTomora <filmrockk@gmail.com>
2022-08-09 19:39:24 +0700

    change header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-lightblue">
-        <p className="navbar-brand">Navbar</p>
+        <p className="navbar-brand">Go-React-todolist by NotTomora</p>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         </button>
       </nav>
